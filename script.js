b3ef270f70dbcc4bdce92ca4dfd582417f24aca2
eloconte <77859601+eloconte@users.noreply.github.com>
2021-03-07 19:05:53 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let colorInput = document.querySelector('#color');
-let hexInput = document.querySelector('#hex');
-
-colorInput.addEventListener('input', () =>{
-	let color = colorInput.value;
-	hexInput.value = color;
-	document.body.style.backgroundColor = color;
-});
