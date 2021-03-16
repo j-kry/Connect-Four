9a65b56c741304cccdd628ea4fd0ff5f44756fb1
alliemaguire <alliemaguire65@gmail.com>
2021-03-15 20:07:40 -0500

    Changes to menu

--- javascript.js
+++ javascript.js
@@ -1,86 +1,113 @@
 //for the game board
 var turn = "red";
-        var player1 = "red";
-        var player2 = "yellow";
-        
-        //Array to keep track of which row has a token placed
-        var indexes = [8, 8, 8, 8, 8, 8, 8, 8];
-        var clickedCol = "";
+var player1 = "red";
+var player2 = "yellow";
 
-        //On mouse enter highlight the column with the player color
-        //$(".column") returns everything with a class name of column
-        $(".column").mouseenter(function() {
+//Array to keep track of which row has a token placed
+var indexes = [8, 8, 8, 8, 8, 8, 8, 8];
+var clickedCol = "";
 
-            if(turn == player1) {
-                this.style.border = "1px solid " + player1;
-                this.style.backgroundColor = "rgba(255, 0, 0, 0.25)";
-            }
-            else {
-                this.style.border = "1px solid " + player2;
-                this.style.backgroundColor = "rgba(255, 255, 0, 0.25)";
-            }
-        });
+//On mouse enter highlight the column with the player color
+//$(".column") returns everything with a class name of column
+$(".column").mouseenter(function() {
 
-        //On mouse leave turn the color back to normal
-        $(".column").mouseleave(function() {
-            this.style.border = "1px solid #000";
-            this.style.backgroundColor = "#1d70ec";
-        });
+    if(turn == player1) {
+        this.style.border = "1px solid " + player1;
+        this.style.backgroundColor = "rgba(255, 0, 0, 0.25)";
+    }
+    else {
+        this.style.border = "1px solid " + player2;
+        this.style.backgroundColor = "rgba(255, 255, 0, 0.25)";
+    }
+});
 
-        //When any column is clicked
-        $(".column").click(function() {
+//On mouse leave turn the color back to normal
+$(".column").mouseleave(function() {
+    this.style.border = "1px solid #000";
+    this.style.backgroundColor = "#1d70ec";
+});
 
-            //Get the id of the column
-            clickedCol = this.id;
-            
-            placeToken(clickedCol, clickedCol.substring(6));
-            
-            changeTurn();
+//When any column is clicked
+$(".column").click(function() {
 
-            //TODO make color change to next players color
-            this.style.border = "1px solid #000";
-            this.style.backgroundColor = "#1d70ec";
+    //Get the id of the column
+    clickedCol = this.id;
 
-        });
+    placeToken(clickedCol, clickedCol.substring(6));
 
-        function changeTurn() {
+    changeTurn();
 
-            if(turn == player1)
-                turn = player2;
-            else
-                turn = player1;
+    //TODO make color change to next players color
+    this.style.border = "1px solid #000";
+    this.style.backgroundColor = "#1d70ec";
 
-        }
+});
 
-        function placeToken(column, num) {
+function changeTurn() {
 
-            //We need to get the id of the bottom-most dot
+    if(turn == player1)
+        turn = player2;
+    else
+        turn = player1;
 
-            //column number --> example "c1"
-            var col = "c" + num;
-            //column's row index --> example "dot1"
-            var row = "dot" + indexes[num-1];
-            //complete id --> example "c1dot1"
-            var together = col + row;
+}
 
-            //place token of the player color
-            document.getElementById(together).style.backgroundColor = turn;
-            //decrement the column index
-            indexes[num-1]--;
+function placeToken(column, num) {
 
-        }
+    //We need to get the id of the bottom-most dot
+
+    //column number --> example "c1"
+    var col = "c" + num;
+    //column's row index --> example "dot1"
+    var row = "dot" + indexes[num-1];
+    //complete id --> example "c1dot1"
+    var together = col + row;
+
+    //place token of the player color
+    document.getElementById(together).style.backgroundColor = turn;
+    //decrement the column index
+    indexes[num-1]--;
+
+}
 //end game board script
-   
+
 //For the modal box on game.html
 // Get the modal
-    var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal");
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
 
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 //end modal box script
+
+// Get the button that opens the modal
+var modal1 = document.getElementById("myModal1");
+var btn1 = document.getElementById("myBtn1");
+
+
+// When the user clicks the button, open the modal 
+function colorFunction() {
+    modal1.style.display = "block";
+}
+
+
+
+
+
+//COLOR PICKER JAVASCRIPT CODE
+let colorInput = document.querySelector('#color');
+let hexInput = document.querySelector('#hex');
+
+colorInput.addEventListener('input', () =>{
+    let color = colorInput.value;
+    hexInput.value = color;
+
+    //changes background color for now
+    document.body.style.backgroundColor = color;
+});
+//end color picker
